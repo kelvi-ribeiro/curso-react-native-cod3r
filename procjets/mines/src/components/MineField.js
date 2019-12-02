ce7f38b4df8978c72da5ce914fab7ce05b6754e7
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-12-01 22:58:39 -0300

    Aula 57. Correção no Componente MineField

--- procjets/mines/src/components/MineField.js
+++ procjets/mines/src/components/MineField.js
@@ -7,14 +7,13 @@ export default ({ board }) => {
     const columns = row.map((field, c) => {
       return <Field {...field} key={c} />
     })
-    return <View key={r}>{columns}</View>
+    return <View style={{flexDirection:'row'}} key={r}>{columns}</View>
   })
   return <View style={styles.container}>{rows}</View>
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flexDirection:'row',
-    backgroundColor:'#EEE'
+  container: {    
+    backgroundColor: '#EEE'
   }
 })
