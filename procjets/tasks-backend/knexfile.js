d21dd877bce2dd1a5bc21007d3ca050e8f951d58
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-12-13 14:58:10 -0300

    Create docker-compose with api and db

--- procjets/tasks-backend/knexfile.js
+++ procjets/tasks-backend/knexfile.js
@@ -3,11 +3,7 @@
 module.exports = {
 
   client: 'postgresql',
-  connection: {
-    database: 'tasks',
-    user: 'adminTasks',
-    password: ''
-  },
+  connection: process.env.DATABASE_URL,
   pool: {
     min: 2,
     max: 10
