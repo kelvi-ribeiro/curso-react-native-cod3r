406f67a7bc7e3ad82ffd94d115118b20ab9ac8f1
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-12-12 23:49:43 -0300

    Aula 83. API: Login de Usuário (Signin)

---
+++ procjets/tasks-backend/api/auth.js
@@ -0,0 +1,31 @@
+const { authSecret } = require('../.env')
+const jwt = require('jwt-simple')
+const bcrypt = require('bcrypt-nodejs')
+
+module.exports = app => {
+  const signin = async (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+      return res.status(400).send('Dados incompletos')
+    }
+    const user = await app.db('users')
+      .where({ email: req.body.email })
+      .first()
+    if (user) {
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if (err || !isMatch) {
+          return res.status(401).send()
+        }
+        const payload = { id: user.id }
+        res.json({
+          name: user.name,
+          email: user.email,
+          token: jwt.encode(payload, authSecret)
+        })
+      })
+    } else {
+      res.status(400).send('Usuário ou senha inválidos')
+    }
+  }
+  return { signin }
+}
