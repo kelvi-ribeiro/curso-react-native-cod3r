5d66899724473dffc20664766ea3aad604ce9935
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-12-17 00:31:59 -0300

    104. Componente Post

--- procjets/lambe/App.js
+++ procjets/lambe/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { View } from 'react-native'
 import Header from './src/components/Header';
+import Post from './src/components/Post';
 
 const App = () => {
   return (
-    <Header />
+    <View style={{ flex: 1 }}>
+      <Header />
+      <Post image={require('./assets/imgs/fence.jpg')}/>
+    </View>
   );
 };
 
