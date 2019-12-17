245d78f031b6a3d8019a0f1362ea4a866d192ba7
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-12-17 00:45:40 -0300

    Aula 106. Componente Comentários

--- procjets/lambe/App.js
+++ procjets/lambe/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native'
 import Header from './src/components/Header';
 import Post from './src/components/Post';
 
-const App = () => {
-  return (
-    <View style={{ flex: 1 }}>
-      <Header />
-      <Post image={require('./assets/imgs/fence.jpg')}/>
-    </View>
-  );
-};
+export default class App extends Component {
+  render() {
+    const comments =
+      [
+        {
+          nickname: 'Kelvi Ribeiro',
+          comment: 'Excelente Foto!'
+        },
+        {
+          nickname: 'Márcia Lima',
+          comment: 'Gostei da foto também. Hehehe'
+        },
+      ]
+    return (
+      <View style={{ flex: 1 }}>
+        <Header />
+        <Post comments={comments} image={require('./assets/imgs/fence.jpg')} />
+      </View>
+    )
 
-
-export default App;
+  };
+}
