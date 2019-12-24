0d3f15d97441c4b1d87ea7b58ec990b11919aef5
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2019-12-24 02:19:31 -0300

    Aula 109. Navegação em Abas

--- procjets/lambe/index.js
+++ procjets/lambe/index.js
@@ -5,5 +5,5 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import Feed from './src/screens/Feed';
-
-AppRegistry.registerComponent(appName, () => Feed);
+import Navigator from './src/Navigator'
+AppRegistry.registerComponent(appName, () => Navigator);
