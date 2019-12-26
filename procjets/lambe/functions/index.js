4b7e0c55e86e875714c1714a205766c839186c88
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2019-12-26 02:57:43 -0300

    Aula 129. Usando Firebase Storage #3

--- procjets/lambe/functions/index.js
+++ procjets/lambe/functions/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
-const cors = require('corsi')({ origin: true })
+const cors = require('cors')({ origin: true })
 const fs = require('fs')
 const uuid = require('uuid-v4')
 const { Storage } = require('@google-cloud/storage')
@@ -10,6 +10,35 @@ const storage = new Storage({
 
 exports.uploadImage = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
+    try {
+      fs.writeFileSync('/tmp/imageTosSave.jpg', request.body.image, 'base64')
+      const bucket = storage.bucket('lambe-cod3r-kelvi.appspot.com')
+      const id = uuid()
+      bucket.upload('/tmp/imageToSave.jpg', {
+        uploadType: 'media',
+        destination: `/posts/${id}.jpg`,
+        metadata: {
+          metadata: {
+            contentType: 'image/jpeg',
+            firebaseStorageDownloadTokens: id
+          }
+        }
 
+      }, (error, file) => {
+        if (error) {
+          console.log(error);
+          return response.status(500).json({ error })
+        } else {
+          const fileName = encodeURIComponent(file.name)
+          const imageUrl = 'https://firebasestorage.googleapis.com/v0/b/'
+            + bucket.name + '/o/' + fileName + '?alt=media&token=' + id
+          return response.status(201).json({ imageUrl: imageUrl })
+        }
+      })
+    } catch (error) {
+      console.log(error);
+      return response.status(500).json({ error })
+
+    }
   })
 });
