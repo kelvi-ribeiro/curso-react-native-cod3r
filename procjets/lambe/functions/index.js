322237f006497cfdc345d90ff25c99cbd506d04d
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2019-12-26 02:37:46 -0300

    Aula 128. Usando Firebase Storage #2

--- procjets/lambe/functions/index.js
+++ procjets/lambe/functions/index.js
@@ -1,7 +1,15 @@
 const functions = require('firebase-functions');
+const cors = require('corsi')({ origin: true })
+const fs = require('fs')
+const uuid = require('uuid-v4')
+const { Storage } = require('@google-cloud/storage')
+const storage = new Storage({
+  projectId: 'lambe-cod3r-kelvi',
+  keyFilename: 'lambe-cod3r-kelvi.json'
+})
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
- exports.helloWorld = functions.https.onRequest((request, response) => {
-  
- });
+exports.uploadImage = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+
+  })
+});
