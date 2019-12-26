e51682663f50b3dba4720b0aeabfdf845ccbb710
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2019-12-26 03:19:27 -0300

    Aula 130. Usando Firebase Storage #4

--- procjets/lambe/functions/index.js
+++ procjets/lambe/functions/index.js
@@ -11,7 +11,7 @@ const storage = new Storage({
 exports.uploadImage = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
     try {
-      fs.writeFileSync('/tmp/imageTosSave.jpg', request.body.image, 'base64')
+      fs.writeFileSync('/tmp/imageToSave.jpg', request.body.image, 'base64')
       const bucket = storage.bucket('lambe-cod3r-kelvi.appspot.com')
       const id = uuid()
       bucket.upload('/tmp/imageToSave.jpg', {
--- procjets/lambe/src/store/actions/posts.js
+++ procjets/lambe/src/store/actions/posts.js
@@ -2,9 +2,22 @@ import { ADD_POST, ADD_COMMENT } from './actionTyes'
 import axios from 'axios'
 export const addPost = post => {
   return dispatch => {
-    axios.post('/posts.json', { ...post })
-      .then(res => console.log(res))
+    // https://us-central1-lambe-cod3r-kelvi.cloudfunctions.net/uploadImage
+    axios({
+      url: 'uploadImage',
+      baseURL: 'https://us-central1-lambe-cod3r-kelvi.cloudfunctions.net',
+      method: 'post',
+      data: {
+        image: post.image.base64
+      }
+    })
       .catch(err => console.log(err))
+      .then(resp => {
+        post.image = resp.data.imageUrl
+        axios.post('/posts.json', { ...post })
+          .then(res => console.log(res))
+          .catch(err => console.log(err))
+      })
   }
   /* return {
     type: ADD_POST,
